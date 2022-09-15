2612f474973a4ecc65f3bb568f263cc0b3adea64
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2022-09-15 14:59:04 +0200

    Adjusted API endpoint name

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,5 @@
-import { reverse, uniqBy } from 'lodash'
 import { data } from '../../data';
 
 export default function handler(req, res) {
-  res.status(200).json(data)
+  res.status(200).json({ data })
 }
