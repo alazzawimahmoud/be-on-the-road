5349a3020e14f6a216c8c2e890c79c46088d4279
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2022-09-15 13:35:12 +0200

    Remove border from menu button

--- components/container.js
+++ components/container.js
@@ -159,7 +159,7 @@ export default function Container({ header, children }) {
 
                         <button
                             type="button"
-                            className="px-4 text-gray-500 border-r border-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+                            className="px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
                             onClick={() => setSidebarOpen(true)}
                         >
                             <span className="sr-only">Open sidebar</span>
