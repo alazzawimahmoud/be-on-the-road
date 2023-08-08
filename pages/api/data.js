e45ae73dab6ddfe7ae631477a2e395bb04c382b6
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2023-08-08 15:45:26 +0200

    Fix wrong import

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,5 @@
 import { data } from '../../data';
-import { data as data2 } from '../../data-2';
 
 export default function handler(req, res) {
-  res.status(200).json({ data, data2 })
+  res.status(200).json({ data })
 }
