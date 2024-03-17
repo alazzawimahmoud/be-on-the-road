006d3bec7d21b9f618a0429b80ff4eda0be95624
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2024-03-17 08:13:24 +0100

    Updated assets base url

--- shared.js
+++ shared.js
@@ -68,4 +68,4 @@ export const ANSWER_TYPES = {
     INPUT: 'INPUT',
 }
 
-export const ASSETS_BASEURL = 'https://res.cloudinary.com/be-on-the-road-assets/image/upload/v1710625875/be-on-the-road-assets'
+export const ASSETS_BASEURL = 'https://storage.googleapis.com/be-on-the-road.appspot.com/files_uuidNames'
